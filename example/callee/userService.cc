8e8b036dc656b72ce3ad54c6d8d6e5232607fc8a
bianyishupian <bianyichips@outlook.com>
2023-07-28 11:12:27 +0800

    example userService

---
+++ example/callee/userService.cc
@@ -0,0 +1,52 @@
+#include <iostream>
+#include <string>
+#include "../user.pb.h"
+#include "../../src/include/rpcProvider.h"
+#include "../../src/include/myrpcApplication.h"
+
+class UserService : public myRPC::UserServiceRpc
+{
+public:
+    bool Login(std::string name, std::string pwd)
+    {
+        std::cout << "doing local service: login" << std::endl;
+        std::cout << "name:" << name << " pwd:" << pwd << std::endl;
+        return false;
+    }
+    bool Register(uint32_t id, std::string name, std::string pwd)
+    {
+        std::cout << "doing local service: Register" << std::endl;
+        std::cout << "id:" << id << "name:" << name << " pwd:" << pwd << std::endl;
+        return true;
+    }
+    void Login(::google::protobuf::RpcController *controller,
+               const ::myRPC::LoginRequest *request,
+               ::myRPC::LoginResponse *response,
+               ::google::protobuf::Closure *done)
+    {
+        std::string name = request->name();
+        std::string pwd = request->pwd();
+
+        bool login_result = Login(name, pwd);
+
+        myRPC::ResultCode *code = response->mutable_result();
+        code->set_errcode(0);
+        code->set_errmsg("");
+        response->set_success(login_result);
+
+        done->Run();
+    }
+
+private:
+};
+
+int main(int argc, char **argv)
+{
+    myrpcApplication::Init(argc, argv);
+
+    RpcProvider provider;
+    provider.NotifyService(new UserService());
+    provider.Run();
+
+    return 0;
+}
