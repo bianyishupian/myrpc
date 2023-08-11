5fe7696443f76f996a4adb852747418a60a5d3cd
bianyishupian <bianyichips@outlook.com>
2023-08-11 12:13:31 +0800

    example caller Friend Service

--- example/caller/callerFriendService.cc
+++ example/caller/callerFriendService.cc
@@ -2,6 +2,7 @@
 #include "rpcApplication.h"
 #include "friend.pb.h"
 #include "rpcChannel.h"
+#include "rpcController.h"
 
 int main(int argc, char **argv)
 {
@@ -14,19 +15,28 @@ int main(int argc, char **argv)
     // rpc method响应
     myRPC::GetFriendsListResponse response;
     // 发起rpc方法的调用 实际上是调用了channel的CallMethod函数 是同步的
-    stub.GetFriendsList(nullptr, &request, &response, nullptr);
-    if (0 == response.result().errcode())
+    MyrpcController controller;
+    stub.GetFriendsList(&controller, &request, &response, nullptr);
+
+    if (controller.Failed())
     {
-        std::cout << "rpc GetFriendsList response success!"  << std::endl;
-        int size = response.friends_size();
-        for (int i = 0; i < size; ++i)
-        {
-            std::cout << "index:" << i << " name:" << response.friends(i) << std::endl;
-        }
+        std::cout << controller.ErrorText() << std::endl;
     }
     else
     {
-        std::cout << "rpc GetFriendsList response error : " << response.result().errmsg() << std::endl;
+        if (0 == response.result().errcode())
+        {
+            std::cout << "rpc GetFriendsList response success!" << std::endl;
+            int size = response.friends_size();
+            for (int i = 0; i < size; ++i)
+            {
+                std::cout << "index:" << i << " name:" << response.friends(i) << std::endl;
+            }
+        }
+        else
+        {
+            std::cout << "rpc GetFriendsList response error : " << response.result().errmsg() << std::endl;
+        }
     }
 
     return 0;
